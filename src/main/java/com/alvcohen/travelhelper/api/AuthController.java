a3e195630340a0ff30923b9a791f3c0e6c523500
Alexey Nesterov <alexey.nesterov@live.com>
2020-06-07 16:01:37 +0100

    Refactor to stateful autoconfigured security

--- src/main/java/com/alvcohen/travelhelper/api/AuthController.java
+++ src/main/java/com/alvcohen/travelhelper/api/AuthController.java
@@ -1,19 +1,11 @@
 package com.alvcohen.travelhelper.api;
 
-import com.alvcohen.travelhelper.config.security.TokenProvider;
-import com.alvcohen.travelhelper.dto.ApiResponse;
-import com.alvcohen.travelhelper.dto.AuthResponse;
-import com.alvcohen.travelhelper.dto.LoginRequest;
 import com.alvcohen.travelhelper.dto.SignUpRequest;
 import com.alvcohen.travelhelper.model.AuthProvider;
 import com.alvcohen.travelhelper.model.User;
 import com.alvcohen.travelhelper.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,35 +20,18 @@ import java.net.URI;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final TokenProvider tokenProvider;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder passwordEncoder, TokenProvider tokenProvider) {
-        this.authenticationManager = authenticationManager;
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.tokenProvider = tokenProvider;
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                loginRequest.getEmail(),
-                loginRequest.getPassword()
-            )
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = tokenProvider.createToken(authentication);
-        return ResponseEntity.ok(new AuthResponse(token));
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("Email: %s already exists", signUpRequest.getEmail()));
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(String.format("Email: %s already exists", signUpRequest.getEmail()));
         }
 
         User user = new User();
@@ -72,8 +47,7 @@ public class AuthController {
             .fromCurrentContextPath().path("/user/me")
             .buildAndExpand(result.getId()).toUri();
 
-        return ResponseEntity.created(location)
-            .body(new ApiResponse(true, String.format("User %s registered successfully", signUpRequest.getEmail())));
+        return ResponseEntity.created(location).build();
     }
 
 
--- src/main/java/com/alvcohen/travelhelper/api/UserController.java
+++ src/main/java/com/alvcohen/travelhelper/api/UserController.java
@@ -5,6 +5,8 @@ import com.alvcohen.travelhelper.model.User;
 import com.alvcohen.travelhelper.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +22,15 @@ public class UserController {
     }
 
     @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("isAuthenticated()")
     public User getCurrentUser(Principal principal) {
         return userRepository.findByEmail(principal.getName())
             .orElseThrow(() -> new ResourceNotFoundException("User", "id", principal.getName()));
+    }
+
+    @GetMapping("/user/token")
+    @PreAuthorize("hasRole('USER')")
+    public String getCurrentUserToken(@AuthenticationPrincipal OidcUser user) {
+        return user.getIdToken().getTokenValue();
     }
 }
